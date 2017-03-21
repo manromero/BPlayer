61594138f538f87614e4c58a7b169b939f597e5a
manromero <miguelangelnunezromero@gmail.com>
2017-03-21 20:12:00 +0100

    Resto de servicios

--- BPlayer/src/main/java/org/restWebService/BPlayer/service/MatchService.java
+++ BPlayer/src/main/java/org/restWebService/BPlayer/service/MatchService.java
@@ -1,5 +1,13 @@
 package org.restWebService.BPlayer.service;
 
+import org.restWebService.BPlayer.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MatchService {
+	
+	@Autowired
+	private MatchRepository matchRepository;
 
 }
--- BPlayer/src/main/java/org/restWebService/BPlayer/service/TeamStatisticsService.java
+++ BPlayer/src/main/java/org/restWebService/BPlayer/service/TeamStatisticsService.java
@@ -1,5 +1,13 @@
 package org.restWebService.BPlayer.service;
 
+import org.restWebService.BPlayer.repository.TeamStatisticsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TeamStatisticsService {
+	
+	@Autowired
+	private TeamStatisticsRepository teamStatisticsRepository;
 
 }
