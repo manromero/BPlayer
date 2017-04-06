ff1f081f56f41d4d49f8c7c39fb0e01c69341e3e
manromero <miguelangelnunezromero@gmail.com>
2017-04-06 19:31:59 +0200

    encuendre rojo login cuando error

--- BPlayerIo2/src/pages/login/login.ts
+++ BPlayerIo2/src/pages/login/login.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { BUserService } from '../../providers/bUserService';
 import { JwtHelper } from 'angular2-jwt';
+
 
 @Component({
 	selector: 'page-login',
@@ -9,51 +11,43 @@ import { JwtHelper } from 'angular2-jwt';
 	providers: [BUserService]
 })
 export class Login {
-	accountCredentials: {username: any, password: any};
+  accountCredentialsForm: FormGroup;
 	jwtHelper: JwtHelper;
 	token: any;
-	
-	constructor(private bUserService: BUserService, public storage: Storage) {
+
+	constructor(private bUserService: BUserService, public storage: Storage, public formBuilder: FormBuilder) {
 		console.log('Constructor Login');
-		this.accountCredentials = {username: 'login', password: 'login'};
+		this.accountCredentialsForm = formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
 		this.jwtHelper = new JwtHelper();
 		//let storage = new Storage();
 		this.storage.get('name').then((name) => {
 			console.log('Me: Hey, ' + name + '! You have a very nice name.');
 		  console.log('You: Thanks! I got it for my birthday.');
 		});
-		
+
 	}
-	
+
 	login(){
 		console.log("A BUser is trying to Login");
-		this.validateAccountCredentials();
-		this.bUserService.login(this.accountCredentials).subscribe(
-			data => {
-				console.log(data);
-			},
-			err => {
-				console.error(err);
-			},
-			() => console.log('LLamada a bUserService.register finalizada')
-		);
-		this.storage.get('id_token').then((token) => {
-			console.log('A new token has been generated: ' + token);
-			console.log(this.jwtHelper.decodeToken(token));
-		});
+		//this.validateAccountCredentials();
+    if(this.accountCredentialsForm.valid) {
+      this.bUserService.login(this.accountCredentialsForm.value).subscribe(
+        data => {
+          console.log(data);
+        },
+        err => {
+          console.error(err);
+        },
+        () => console.log('LLamada a bUserService.register finalizada')
+      );
+      this.storage.ready().then(()=>
+        this.storage.get('id_token').then((token) => {
+          console.log('A new token has been generated: ' + token);
+          console.log(this.jwtHelper.decodeToken(token));
+      }));
+    }
 	}
-	
-	validateAccountCredentials(){
-		if(this.accountCredentials!=null){
-			if(this.accountCredentials.username==null || this.accountCredentials.username==''){
-				console.log('Username can not be empty');
-			}
-			if(this.accountCredentials.password==null || this.accountCredentials.password==''){
-				console.log('Password can not be empty');
-			}
-		}else{
-			console.log('Credentials can not be null');
-		}
-	}
-		
 }
