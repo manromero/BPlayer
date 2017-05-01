7a580ba85e5e422d856cffaa6814e2f915f740a0
manromero <miguelangelnunezromero@gmail.com>
2017-05-01 11:19:16 +0200

    se comienza el cliente de detalles de grupo

--- BPlayerIo2/src/pages/detailsOrganization/detailsOrganization.ts
+++ BPlayerIo2/src/pages/detailsOrganization/detailsOrganization.ts
@@ -3,6 +3,7 @@ import { Storage } from '@ionic/storage';
 import { NavController, NavParams } from 'ionic-angular';
 import { JwtHelper } from 'angular2-jwt';
 import { HelloIonicPage } from '../hello-ionic/hello-ionic';
+import { CreateTeam } from '../createTeam/createTeam';
 import { OrganizationService } from '../../providers/organizationService';
 
 @Component({
@@ -13,6 +14,7 @@ import { OrganizationService } from '../../providers/organizationService';
 export class DetailsOrganization {
 
   organization: any;
+  listBUserToBeAdministrator: any;
   jwtHelper: JwtHelper;
 
 	constructor(public storage: Storage, public navCtrl: NavController, public navParams: NavParams, private organizationService: OrganizationService) {
@@ -24,7 +26,7 @@ export class DetailsOrganization {
         if(this.jwtHelper.isTokenExpired(token)){
           this.navCtrl.setRoot(HelloIonicPage);
         }else{
-          
+
           //LLamada al provider para recuperar los datos de la organization
           this.organizationService.findDetailedOrganizationByIdOrganization(navParams.get('idOrganization'), token).subscribe(data => this.organization = data);
 
@@ -32,5 +34,15 @@ export class DetailsOrganization {
 
       }));
 	}
+
+  //Va a la pagina de creacion de Team
+  goToCreateTeamPage(){
+    this.navCtrl.push(CreateTeam);
+  }
+
+  //Find BUser that could be a new administrator
+  findListBUserToBeAdministrator(){
+	  console.log("Find BUser that could be a new administrator");
+  }
 
 }
